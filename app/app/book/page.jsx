9a305c221f7b5755f258d278db5bb59bf01c5571
TrustNo1ever007 <admin@oursocialimage.net>
2025-08-13 20:14:34 -0400

    Update page.jsx

--- app/app/book/page.jsx
+++ app/app/book/page.jsx
@@ -8,7 +8,7 @@ export default function Page() {
       <div style={{ minWidth: '320px', height: '700px', width: '100%' }}>
         <div
           className="calendly-inline-widget"
-          data-url="https://calendly.com/admin-oursocialimage/30min"
+          data-url="https://calendly.com/admin-oursocialimage/30min?hide_gdpr_banner=1"
           style={{ minWidth: '320px', height: '100%' }}
         ></div>
       </div>
